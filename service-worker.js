0af8a1b1b3f70574936b73d9c9f13d9cea552cf7
Carlin Mackenzie <carlin.mackenzie@gmail.com>
2019-01-13 02:58:34 +0000

    testing progressive web app stuff and experimentation with service workers

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open('paperparachutes').then(function (cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/style.css',
+                '/PaperParachutes.js',
+                '/resources/'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    console.log(event.request.url);
+});
+
+self.addEventListener('fetch', function (event) {
+    console.log(event.request.url);
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+});
